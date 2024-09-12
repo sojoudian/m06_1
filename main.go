5a6324cd92313c178d70f19ac23d5f0a2449ed2e
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-12 14:43:15 -0400

    change port

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", helloHandler) // Register the handler for the root path
 
-	fmt.Println("Starting server on :5000")
-	err := http.ListenAndServe(":5000", nil) // Start the server on port 8080
+	fmt.Println("Starting server on :5001")
+	err := http.ListenAndServe(":5001", nil) // Start the server on port 8080
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
