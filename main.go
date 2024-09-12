295a2dca681dac2c9d795420a83e3980d7498d71
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-12 14:41:38 -0400

    add main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello class WINP2000")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler) // Register the handler for the root path
+
+	fmt.Println("Starting server on :5000")
+	err := http.ListenAndServe(":5000", nil) // Start the server on port 8080
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
